b6915bdf198e33cc6c4873776c3e46f113d41e79
ljpinnell <84143512+ljpinnell@users.noreply.github.com>
2022-09-21 12:58:24 -0400

    Add files via upload

---
+++ 16S_Analysis/g_unifrac.R
@@ -0,0 +1,9 @@
+### function to calculate generalized UniFrac using GUniFrac package
+library(GUniFrac, quietly = T)
+
+gunifrac <- function(x) {
+  y <- GUniFrac(as(t(otu_table(x)), "matrix"), phy_tree(x), alpha = c(0.5))$unifracs
+  z <- y[, ,"d_0.5"]
+  z.d <- as.dist(z)
+  
+}
---
+++ 16S_Analysis/w_unifrac.R
@@ -0,0 +1,9 @@
+### function to calculate generalized UniFrac using GUniFrac package
+library(GUniFrac, quietly = T)
+
+wunifrac <- function(x) {
+  y <- GUniFrac(as(t(otu_table(x)), "matrix"), phy_tree(x), alpha = c(1))$unifracs
+  z <- y[, ,"d_1"]
+  z.d <- as.dist(z)
+  
+}
